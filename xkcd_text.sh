c530f88e288c307d9399e1ba026270af8588f56b
Yash Srivastav <yash111998@gmail.com>
2016-11-19 02:04:03 +0530

    Remove Redundant stuff

--- xkcd_text.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-num=$(cat ~/.current_comic)
-num=${num%\.*}
-num=${num##*/}
-file=$num".dat"
-values=$(cat ~/XKCD/data/$file | tail -n 1)
-echo $values
--- xkcd_title.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-num=$(cat ~/.current_comic)
-num=${num%\.*}
-num=${num##*/}
-file=$num".dat"
-values=$(cat ~/XKCD/data/$file | head -n 1)
-echo $values
