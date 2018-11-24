96d91e8f5871ac04af011a9c50bed752ab826875
Yash Srivastav <yash111998@gmail.com>
2018-11-24 15:09:20 +0530

    awesome updates

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -9,4 +9,5 @@ function run {
 
 run xautolock -detectsleep -time 3 -locker "/home/yash/bin/lock.sh" -notify 30 -notifier "/home/yash/bin/lock-notify.sh"
 run xss-lock -- "/home/yash/bin/lock.sh"
-run emacs --daemon
+#run emacs --daemon
+run /home/yash/bin/disable-autolock 2>&1 1> /tmp/disable-logs
