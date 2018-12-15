53af2311436fbf111dd23cb94c516e529fec04ab
Yash Srivastav <yash111998@gmail.com>
2018-12-15 23:11:50 +0530

    Remove awesome from main dotfiles

--- awesome/conf/widgets/memory.lua
+++
@@ -1,6 +0,0 @@
-local wibox = require("wibox")
-local vicious = require("vicious")
-memwidget = wibox.widget.textbox()
-vicious.cache(vicious.widgets.mem)
-vicious.register(memwidget, vicious.widgets.mem, "($2MB/$3MB) ", 13)
-return memwidget
--- awesome/walls.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-var=$(ls -d -1 /home/yash/.bing-wallpapers/special/*.* | sort -R | head -n1)
-echo -n "$var"
